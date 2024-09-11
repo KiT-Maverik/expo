0c2d804449c78fb6f915906e4ac2876ba122b1f1
kit <kit.mavareik@gmail.com>
2024-09-11 14:59:36 +0300

    Add JWT parsing utility
    Introduced `parseJwt` function in `helpers/` to decode JWT tokens. Updated `index.ts` to export the new utility function, enhancing token handling capabilities.

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,6 +4,9 @@ export * from "./converters/pxToRem.util.ts";
 // Dates
 export * from "./dates/getUtcDateFromIso.util.ts";
 
+// Helpers
+export * from "./helpers/parseJwt.util.ts";
+
 // Numbers
 export * from "./numbers/prettifyNumber.util.ts";
 export * from "./numbers/simplifyNumber.util.ts";
