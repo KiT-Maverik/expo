73aab9ede89208da8d9ee38798a114bdac2b4607
kit <kit.mavareik@gmail.com>
2024-09-11 14:20:15 +0300

    Remove unused state and component from App
    Refactored App component to remove the unused `locale` state and `CvPage` import. Simplified the `AppSettings` context to include only a sample property.

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,38 +1,32 @@
-import React, { createContext, useMemo, useState } from "react";
+import { createContext, useMemo } from "react";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import { CssBaseline } from "@mui/material";
-import {CvPage} from "design/pages";
 
 import { ThemeProvider } from "./utilitary/ThemeProvider/ThemeProvider";
 import { ToastProvider } from "./utilitary/ToastProvider/ToastProvider";
-import { LOCALE } from "../constants";
 
 interface AppSettings {
-  locale: LOCALE;
-  setLocale: (locale: LOCALE) => void;
+    sample: string
 }
+
 export const AppSettings = createContext({} as AppSettings);
 
 export const App = () => {
-  const [locale, setLocale] = useState<LOCALE>(LOCALE.EN);
 
-  const contextValue = useMemo<AppSettings>(
-    () => ({
-      locale,
-      setLocale: (locale: LOCALE) => setLocale(locale),
-    }),
-    [locale],
+  const appSettings = useMemo<AppSettings>(
+    () => ({sample: 'sample'}),
+    [],
   );
 
   return (
-    <AppSettings.Provider value={contextValue}>
+    <AppSettings.Provider value={appSettings}>
       <CssBaseline />
       <ThemeProvider>
         <ToastProvider>
-          <CvPage />
+            pew
         </ToastProvider>
       </ThemeProvider>
     </AppSettings.Provider>
