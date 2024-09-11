287bc97c584062e7a518c8988a32b522ad019bd4
kit <kit.mavareik@gmail.com>
2024-09-11 14:46:01 +0300

    Add utility functions for number formatting and date conversion
    Introduce `simplifyNumber` and `prettifyNumber` utilities for number formatting, enhancing readability of large numbers. Additionally, add `getUtcDateFromIso` for converting ISO date strings to UTC. Update index.ts to export newly added utilities for streamlined integration.

---
+++ src/utils/dates/getUtcDateFromIso.util.ts
@@ -0,0 +1,7 @@
+import { addMinutes } from 'date-fns'
+
+export const getUtcDateFromIso = (dateString: string) => {
+    const date = new Date(dateString)
+    const offset = date.getTimezoneOffset()
+    return addMinutes(date, offset)
+}
---
+++ src/utils/index.ts
@@ -0,0 +1,9 @@
+// Converters
+export * from "./converters/pxToRem.util.ts";
+
+// Dates
+export * from "./dates/getUtcDateFromIso.util.ts";
+
+// Numbers
+export * from "./numbers/prettifyNumber.util.ts";
+export * from "./numbers/simplifyNumber.util.ts";
