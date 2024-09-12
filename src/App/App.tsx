8423fc6caf1abf413e798990fc8d9c7bfb59def6
kit <kit.mavareik@gmail.com>
2024-09-12 18:55:09 +0300

    Add GlobalStyles and Router to App component
    Integrate GlobalStyles for consistent styling across the application. Add Router component to manage application routing more effectively.

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -3,15 +3,17 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import { CssBaseline } from "@mui/material";
+import { CssBaseline, GlobalStyles } from "@mui/material";
+
+import { globalStyles } from "styles";
 
 import { ThemeProvider } from "./utilitary/ThemeProvider/ThemeProvider";
 import { ToastProvider } from "./utilitary/ToastProvider/ToastProvider";
+import { Router } from "./utilitary/Router/Router";
 
 interface AppSettings {
   sample: string;
 }
-
 export const AppSettings = createContext({} as AppSettings);
 
 export const App = () => {
@@ -21,6 +23,8 @@ export const App = () => {
     <AppSettings.Provider value={appSettings}>
       <CssBaseline />
       <ThemeProvider>
+        <GlobalStyles styles={globalStyles} />
+        <Router />
         <ToastProvider>pew</ToastProvider>
       </ThemeProvider>
     </AppSettings.Provider>
