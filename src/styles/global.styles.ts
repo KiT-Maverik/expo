414cdd4305022a798470ff1388a2980782b7247e
kit <kit.mavareik@gmail.com>
2024-09-13 17:16:34 +0300

    Refactor global styles to utilize theme properties
    Updated globalStyles to be a function that accepts the theme object and applies the background color dynamically based on the theme's palette. This ensures better alignment with theme customization and improves overall styling consistency.

--- src/styles/global.styles.ts
+++ src/styles/global.styles.ts
@@ -1,10 +1,12 @@
 import { Interpolation } from "@mui/material";
 import { Theme } from "@mui/material/styles";
 
-export const globalStyles: Interpolation<Theme> = {
-  "#root": {
-    display: "flex",
-    flexDirection: "column",
-    minHeight: "100vh",
-  },
-} as const;
+export const globalStyles: Interpolation<Theme> = (theme) =>
+  ({
+    "#root": {
+      display: "flex",
+      flexDirection: "column",
+      minHeight: "100vh",
+      backgroundColor: theme.palette.background.surface,
+    },
+  }) as const;
