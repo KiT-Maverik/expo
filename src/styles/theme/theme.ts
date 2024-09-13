8753083fecafa34ac316c7e6a5e20c95c17601ba
kit <kit.mavareik@gmail.com>
2024-09-13 15:28:34 +0300

    Add breakpoints to theme configuration
    Created a new breakpoints configuration file and integrated it into the theme setup. This provides standard breakpoint values for mobile, tablet, laptop, desktop, and fullWidth screens, enhancing the responsiveness of the application.

--- src/styles/theme/theme.ts
+++ src/styles/theme/theme.ts
@@ -1,6 +1,7 @@
 import { createTheme, PaletteMode } from "@mui/material/styles";
 
 import * as components from "./components";
+import { breakpoints } from "./breakpoints";
 import { paletteLight } from "./paletteLight";
 import { paletteDark } from "./paletteDark";
 import { shape } from "./shape";
@@ -8,9 +9,10 @@ import { typography } from "./typography";
 
 export const theme = (mode: PaletteMode) =>
   createTheme({
+    breakpoints,
     components,
     palette: mode === "light" ? paletteLight : paletteDark,
     shape,
-    typography,
     spacing: 4,
+    typography,
   });
