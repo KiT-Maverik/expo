4a57a9914deafbc29282992f8b3dee1be98855b7
kit <kit.mavareik@gmail.com>
2024-09-13 15:50:10 +0300

    Add DemoBanner to BannerProvider
    Replaced the existing banners with a single DemoBanner for testing. Removed unnecessary imports and redundant banner conditions to streamline the component.

--- src/App/utilitary/BannerProvider/BannerProvider.tsx
+++ src/App/utilitary/BannerProvider/BannerProvider.tsx
@@ -1,50 +1,18 @@
-import { Box } from '@mui/material'
-import { useMemo } from 'react'
-import { useLocation } from 'react-router-dom'
+import { Box } from "@mui/material";
+import { useMemo } from "react";
 
-import { useAppSelector } from 'App'
-import { useCustomerQuery, useCustomerSubscriptionQuery } from 'api/queries'
-import { ActingAs, LockedAccount, TrialExpiration, UploadLimit } from 'design/organisms/BannerProvider'
-import { selectAccount } from 'design/pages/AccountSettings/account.slice'
-import useIsLockedAccount from 'hooks/user/useIsLockedAccount'
-import { CustomerStatus } from 'types/Customer'
-import { useUploadLimit } from 'hooks/user/useUploadLimit.hook'
-import { route } from 'constants/routes'
+import { DemoBanner } from "./banners/DemoBanner/DemoBanner.tsx";
 
 export const BannerProvider = () => {
-    const { isAgency } = useAppSelector(selectAccount)
-    const { pathname } = useLocation()
+  const content = useMemo(() => {
+    const result = [];
 
-    const { data: customer } = useCustomerQuery()
-    const { data: subscription } = useCustomerSubscriptionQuery()
-    const isLockedAccount = useIsLockedAccount()
-    const { uploadLimitReached } = useUploadLimit(false)
+    if (true) result.push(<DemoBanner />);
 
-    const showTrialExpirationBanner = useMemo(
-        () =>
-            customer?.status === CustomerStatus.trial &&
-            !subscription?.creditCard &&
-            pathname === route.settings.account.index,
-        [pathname, subscription?.creditCard, customer?.status],
-    )
+    return result;
+  }, []);
 
-    const enableUploadLimitBanner = useMemo(
-        () => uploadLimitReached && pathname === route.upload && isAgency,
-        [isAgency, uploadLimitReached, pathname],
-    )
+  if (content.length === 0) return null;
 
-    const content = useMemo(() => {
-        const result = []
-
-        if (isLockedAccount) result.push(<LockedAccount key="locked account" />)
-        if (enableUploadLimitBanner) result.push(<UploadLimit key="upload limit" />)
-        if (showTrialExpirationBanner) result.push(<TrialExpiration key="trial expiration" />)
-        if (isAgency && pathname !== route.agency.dashboard) result.push(<ActingAs key="acting as client" />)
-
-        return result
-    }, [showTrialExpirationBanner, isLockedAccount, isAgency, pathname, enableUploadLimitBanner])
-
-    if (content.length === 0) return null
-
-    return <Box>{content}</Box>
-}
+  return <Box>{content}</Box>;
+};
