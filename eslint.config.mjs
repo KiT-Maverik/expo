011cfce0a64c6c78d5d755652f8db5b7e2cb3bbf
kit <kit.mavareik@gmail.com>
2024-09-13 21:56:47 +0300

    Update ESLint config and dependencies
    Added eslint-plugin-todo-plz and updated eslint-plugin-import. Enhanced ESLint rules for better import management and ticket reference enforcement.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,9 +6,32 @@ import pluginPrettier from "eslint-plugin-prettier/recommended";
 
 export default [
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   pluginPrettier,
+  {
+    languageOptions: { globals: globals.browser },
+    rules: {
+      'import/consistent-type-specifier-style': ['error', 'prefer-inline'],
+      'import/newline-after-import': ['error', { count: 1 }],
+      'import/no-duplicates': ['error', { 'prefer-inline': true }],
+      'import/order': [
+        'error',
+        {
+          groups: ['builtin', 'external', 'internal', 'type', 'parent', 'sibling', 'object', 'index'],
+          pathGroupsExcludedImportTypes: [],
+        },
+      ],
+      'todo-plz/ticket-ref': [
+        'error',
+        {
+          commentPattern: 'TODO:\\s\\[(T-[0-9]+[,\\s]*)+\\]',
+          description: 'Example: TODO: [T-123] Lorem ipsum dolor sit amet',
+        },
+      ],
+      "react/react-in-jsx-scope": "off",
+      'react/prop-types': 'off',
+    },
+  },
 ];
