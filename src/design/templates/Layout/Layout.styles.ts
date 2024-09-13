31b685b46882152328fa95e25595505e1badb47b
kit <kit.mavareik@gmail.com>
2024-09-13 20:55:16 +0300

    Add drawer functionality to Layout component
    Introduced a persistent drawer with open/close actions, dynamically setting its vertical offset. Updated Layout styles to support the drawer and adjusted the toolbar to handle drawer state.

--- src/design/templates/Layout/Layout.styles.ts
+++ src/design/templates/Layout/Layout.styles.ts
@@ -1,6 +1,8 @@
 import { SxProps } from "@mui/material";
 import { Theme } from "@mui/material/styles";
 
+const drawerWidth = 240;
+
 const main: SxProps<Theme> = { flexGrow: 1, display: "grid" } as const;
 
 const loader: SxProps<Theme> = {
@@ -10,7 +12,25 @@ const loader: SxProps<Theme> = {
   bottom: 0,
 } as const;
 
+const drawer: {
+  container: (offset: number) => SxProps<Theme>;
+  header: SxProps<Theme>;
+} = {
+  container: (offset) => ({
+    width: drawerWidth,
+    top: offset - 1,
+  }),
+  header: (theme) => ({
+    display: "flex",
+    alignItems: "center",
+    padding: theme.spacing(0, 1),
+    ...theme.mixins.toolbar,
+    justifyContent: "flex-end",
+  }),
+} as const;
+
 export default {
+  drawer,
   loader,
   main,
 };
--- src/design/templates/Layout/Layout.tsx
+++ src/design/templates/Layout/Layout.tsx
@@ -4,25 +4,50 @@ import {
   AppBar,
   Box,
   Button,
+  Divider,
+  Drawer,
   IconButton,
   LinearProgress,
   Toolbar,
   Typography,
 } from "@mui/material";
 
-import { BannerProvider, useAppSelector, selectAppState } from "App";
+import {
+  BannerProvider,
+  useAppSelector,
+  selectAppState,
+  useAppDispatch,
+  closeDrawer,
+  openDrawer,
+} from "App";
 
 import style from "./Layout.styles";
+import { useEffect, useRef, useState } from "react";
 
 export const Layout = () => {
-  const { showLoader } = useAppSelector(selectAppState);
+  const [drawerVerticalOffset, setDrawerVerticalOffset] = useState(0);
+  const toolbarRef = useRef<HTMLDivElement | null>(null);
+  const { showLoader, showDrawer } = useAppSelector(selectAppState);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (toolbarRef.current)
+      setDrawerVerticalOffset(toolbarRef.current.getBoundingClientRect().top);
+  }, []);
 
   return (
     <>
       <BannerProvider />
       <AppBar position="relative">
-        <Toolbar>
-          <IconButton size="large" edge="start" color="inherit">
+        <Toolbar ref={toolbarRef}>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            onClick={() =>
+              showDrawer ? dispatch(closeDrawer()) : dispatch(openDrawer())
+            }
+          >
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" component="div" flexGrow={1}>
@@ -32,6 +57,19 @@ export const Layout = () => {
         </Toolbar>
         {showLoader && <LinearProgress sx={style.loader} />}
       </AppBar>
+      <Drawer
+        PaperProps={{ sx: style.drawer.container(drawerVerticalOffset) }}
+        variant='persistent'
+        anchor="left"
+        open={showDrawer}
+        onClose={() => dispatch(closeDrawer())}
+      >
+        <Box sx={style.drawer.header}>
+          <IconButton onClick={() => console.log(1)}>pew</IconButton>
+        </Box>
+        <Divider />
+        pepewpew
+      </Drawer>
       <Box component="main" sx={style.main}>
         {<Outlet />}
       </Box>
