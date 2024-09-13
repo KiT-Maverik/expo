45b46375528e71313e75a20ad1e8847b13068210
kit <kit.mavareik@gmail.com>
2024-09-13 15:39:02 +0300

    Add FullscreenModal component
    Introduce a new FullscreenModal component with a modal layout that takes up maximum screen space. Also, re-organize modal exports for better clarity.

--- src/design/templates/index.ts
+++ src/design/templates/index.ts
@@ -3,5 +3,6 @@ export * from "./Layout/Layout";
 // Modals
 export * from "./Modal/Modal";
 export * from "./Modal/Modal.hooks";
-export * from "./Modal/templates/SimpleModal/SimpleModal.tsx";
+export * from "./Modal/templates/FullscreenModal/FullscreenModal";
 export * from "./Modal/templates/ComplexModal/ComplexModal";
+export * from "./Modal/templates/SimpleModal/SimpleModal.tsx";
