934303fafc1fb2cd8461aa76fa483cc3a2190ba6
kit <kit.mavareik@gmail.com>
2024-09-13 15:49:49 +0300

    Add DemoBanner component and export BannerProvider
    Introduced the DemoBanner component for informational alerts with embedded actions. Updated index.ts file to export the new BannerProvider, ensuring it is available for use throughout the application.

--- src/App/index.ts
+++ src/App/index.ts
@@ -1,3 +1,4 @@
 export * from "./utilitary/ModalProvider/ModalProvider.slice.ts";
+export * from "./utilitary/BannerProvider/BannerProvider.tsx";
 export * from "./App";
-export * from "./App.store.ts"
+export * from "./App.store.ts";
---
+++ src/App/utilitary/BannerProvider/banners/DemoBanner/DemoBanner.tsx
@@ -0,0 +1,22 @@
+import { Alert, Button } from "@mui/material";
+import LogIcon from "@mui/icons-material/SmsFailedRounded";
+
+export const DemoBanner = () => {
+  return (
+    <Alert
+      severity="info"
+      variant="standard"
+      action={
+        <Button
+          variant="outlined"
+          startIcon={<LogIcon />}
+          onClick={() => console.log('Demo banner says: "Hey!"')}
+        >
+          Log message
+        </Button>
+      }
+    >
+      Lorem ipsum dolor sit amet.
+    </Alert>
+  );
+};
