37035d98266d194f981d5906ea5deb41b777124c
kit <kit.mavareik@gmail.com>
2024-09-17 12:01:09 +0300

    Fix import alias for TypeScript ESLint configurations
    The alias for the TypeScript ESLint import was adjusted from 'tseslint' to 'tsEsLint'. This change ensures consistency with the naming conventions used elsewhere in the codebase and prevents potential issues with import resolution.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,13 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import tsEsLint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import pluginPrettier from "eslint-plugin-prettier/recommended";
 
 export default [
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  ...tsEsLint.configs.recommended,
   pluginReact.configs.flat.recommended,
   pluginPrettier,
   {
