4ee9c847a0042186cfdb14b401fe64a57c93f159
kit <kit.mavareik@gmail.com>
2024-09-17 19:06:42 +0300

    Add localization support for inputs, dates, operations, and messages
    This commit introduces new localization files for inputs, dates, operations, and messages in both English and Ukrainian. These files will help in displaying appropriate labels, placeholders, and messages based on the selected language. The changes are part of efforts to make the application multilingual.

---
+++ src/locales/generic.locale.ts
@@ -0,0 +1,11 @@
+import { dates } from "./generic/dates.ts";
+import { inputs } from "./generic/inputs.ts";
+import { operations } from "./generic/operations";
+import { messages } from "./generic/messages";
+
+export const generic = {
+  dates,
+  inputs,
+  messages,
+  operations,
+} as const;
---
+++ src/locales/generic/inputs.ts
@@ -0,0 +1,127 @@
+const en = {
+  creditCard: {
+    number: {
+      label: "Card Number",
+      placeholder: "**** **** **** ****",
+    },
+    cvv: {
+      label: "CVV",
+      placeholder: "•••",
+    },
+    expiry: {
+      month: {
+        label: "Month",
+        placeholder: "MM",
+      },
+      year: {
+        label: "Year",
+        placeholder: "YYYY",
+      },
+    },
+  },
+  user: {
+    email: {
+      label: "Email",
+      placeholder: "Enter your email",
+    },
+    name: {
+      label: "Name",
+      placeholder: "Enter your name",
+    },
+    firstName: {
+      label: "First Name",
+      placeholder: "Enter your name",
+    },
+    lastName: {
+      label: "Last Name",
+      placeholder: "Enter your last name",
+    },
+    password: {
+      label: "Password",
+      placeholder: "Enter your password",
+    },
+    newPassword: {
+      label: "New Password",
+      placeholder: "Enter your new password",
+    },
+    confirmPassword: {
+      label: "Confirm Password",
+      placeholder: "Enter your password confirmation",
+    },
+  },
+  address: {
+    zip: {
+      label: "ZIP Code",
+      placeholder: "#####",
+    },
+  },
+  common: {
+    placeholder: "Type here...",
+  },
+};
+
+export type InputsLocale = typeof en;
+
+const ua: InputsLocale = {
+  creditCard: {
+    number: {
+      label: "Номер картки",
+      placeholder: "**** **** **** ****",
+    },
+    cvv: {
+      label: "CVV",
+      placeholder: "•••",
+    },
+    expiry: {
+      month: {
+        label: "Місяць",
+        placeholder: "ММ",
+      },
+      year: {
+        label: "Рік",
+        placeholder: "РРРР",
+      },
+    },
+  },
+  user: {
+    email: {
+      label: "Електронна пошта",
+      placeholder: "Введіть вашу електронну пошту",
+    },
+    name: {
+      label: "Ім'я",
+      placeholder: "Введіть ваше ім'я",
+    },
+    firstName: {
+      label: "Ім'я",
+      placeholder: "Введіть ваше ім'я",
+    },
+    lastName: {
+      label: "Прізвище",
+      placeholder: "Введіть ваше прізвище",
+    },
+    password: {
+      label: "Пароль",
+      placeholder: "Введіть ваш пароль",
+    },
+    newPassword: {
+      label: "Новий пароль",
+      placeholder: "Введіть ваш новий пароль",
+    },
+    confirmPassword: {
+      label: "Підтвердження паролю",
+      placeholder: "Введіть підтвердження паролю",
+    },
+  },
+  address: {
+    zip: {
+      label: "Поштовий індекс",
+      placeholder: "#####",
+    },
+  },
+  common: {
+    placeholder: "Введіть тут...",
+  },
+};
+
+export const inputs = { en, ua };
