d0e3d3fee98481a051e59787a7e920af4d2a2d3d
kit <kit.mavareik@gmail.com>
2024-09-17 19:49:25 +0300

    Refactor locale structure to support dynamic language loading
    Reorganized locale files to support dynamic language-based loading. Implemented functions to retrieve localized data based on the specified language, aligning the code with our internationalization standards. This change enhances the flexibility and scalability of locale management in the application.

--- src/locales/generic.locale.ts
+++ src/locales/generic.locale.ts
@@ -1,11 +1,14 @@
-import { dates } from "./generic/dates.ts";
-import { inputs } from "./generic/inputs.ts";
-import { operations } from "./generic/operations";
-import { messages } from "./generic/messages";
+import { LANGUAGE } from "App";
 
-export const generic = {
-  dates,
-  inputs,
-  messages,
-  operations,
-} as const;
+import { dates } from "./global/dates.ts";
+import { inputs } from "./global/inputs.ts";
+import { operations } from "./global/operations";
+import { messages } from "./global/messages";
+
+export const getGenericLocale = (language: LANGUAGE) =>
+  ({
+    dates: dates[language],
+    inputs: inputs[language],
+    messages: messages[language],
+    operations: operations[language],
+  }) as const;
---
+++ src/locales/index.ts
@@ -0,0 +1,7 @@
+import { LANGUAGE } from "App";
+import { getGenericLocale } from "./generic.locale.ts";
+
+export const getLocale = (language: LANGUAGE) =>
+  ({
+    generic: getGenericLocale(language),
+  }) as const;
