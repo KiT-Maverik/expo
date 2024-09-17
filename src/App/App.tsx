a680d35ab4ab1de7980329e1f73094e67c9d3ab1
kit <kit.mavareik@gmail.com>
2024-09-17 19:50:41 +0300

    Add i18n support for English and Ukrainian languages
    Integrated i18n with `react-i18next` for locale management. Added language detection via localStorage and default fallback to English. Updated `AppSettings` context to include the language setting.

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,10 +1,12 @@
-import { createContext, useMemo } from "react";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import { Provider as StoreProvider } from "react-redux";
 import { CssBaseline, GlobalStyles } from "@mui/material";
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import { createContext, useMemo } from "react";
+import { Provider as StoreProvider } from "react-redux";
 
 import { globalStyles } from "styles";
 
@@ -13,14 +15,40 @@ import { ToastProvider } from "./ToastProvider/ToastProvider";
 import { ModalProvider } from "./ModalProvider/ModalProvider.tsx";
 import { Router } from "./Router/Router";
 import { appStore } from "./App.store.ts";
+import { LANGUAGE } from "./variables/environment.constants.ts";
+import { localStorageKey } from "./variables/local-storage.constants.ts";
+import { getLocale } from "locales";
 
 interface AppSettings {
-  sample: string;
+  language: string;
 }
 export const AppSettings = createContext({} as AppSettings);
 
 export const App = () => {
-  const appSettings = useMemo<AppSettings>(() => ({ sample: "sample" }), []);
+  const appSettings = useMemo<AppSettings>(() => ({ language: "sample" }), []);
+
+  i18n.use(initReactI18next).init({
+    resources: {
+      [LANGUAGE.ENGLISH]: getLocale(LANGUAGE.ENGLISH),
+      [LANGUAGE.UKRAINIAN]: getLocale(LANGUAGE.UKRAINIAN),
+    },
+    lng: (() => {
+      const localeKey = localStorage.getItem(localStorageKey.language);
+
+      if (
+        !localeKey ||
+        !Object.values(LANGUAGE).includes(localeKey as LANGUAGE)
+      ) {
+        localStorage.setItem(localStorageKey.language, LANGUAGE.ENGLISH);
+        return LANGUAGE.ENGLISH;
+      } else return localeKey;
+    })(),
+    fallbackLng: LANGUAGE.ENGLISH,
+
+    interpolation: {
+      escapeValue: false,
+    },
+  });
 
   return (
     <AppSettings.Provider value={appSettings}>
