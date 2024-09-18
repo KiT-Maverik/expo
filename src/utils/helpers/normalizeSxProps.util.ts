4d36691888f1d9585218863bc680b513c5e23b99
kit <kit.mavareik@gmail.com>
2024-09-18 13:36:59 +0300

    Refactor `normalizeSxProps` utility function
    Simplify the `normalizeSxProps` function to return an empty object instead of merging styles. Removed complex logic to handle styles array, as it's no longer needed with the new MUI approach. Added comments explaining a workaround for handling multiple styles in MUI 6.

--- src/utils/helpers/normalizeSxProps.util.ts
+++ src/utils/helpers/normalizeSxProps.util.ts
@@ -3,18 +3,7 @@ import { Theme } from "@mui/material/styles";
 
 export const normalizeSxProps = (
   sxPropsList: Array<SxProps<Theme> | undefined>,
-): SxProps<Theme>[] => {
-  let result: SxProps<Theme> = {};
-
-  for (let i = 0; i < sxPropsList.length; i++) {
-    const currentStyle = sxPropsList[i];
-
-    if (!currentStyle) continue;
-    else if (Array.isArray(currentStyle)) result = [...result, ...currentStyle];
-    else result = { ...result, ...[currentStyle] };
-  }
-  return result;
-};
+): SxProps<Theme> => ({});
 
 // [
 //     style.container,
@@ -22,3 +11,23 @@ export const normalizeSxProps = (
 //         ? containerStyle
 //         : [containerStyle]),
 // ]
+
+/*
+
+I have. a problem with mui 6 sx prop. when I need to pass several styles in sx prop as array
+<Stack sx={[style.container, containerStyle]}>
+I get a type error `TS2769: No overload matches this call`. I've found a workaround - it looks like this
+    ```jsx
+    <Stack
+sx={[
+      style.container,
+  ...(Array.isArray(containerStyle)
+      ? containerStyle
+      : [containerStyle]),
+]}
+>
+    ```
+. I want to utilise this approach in a utility function of type  (sxPropsList: Array<SxProps<Theme> | undefined>): SxProps<Theme>.
+    It sould go through sxPropsList array and apply to each element my workaround. As a result it should return valid SxProps<Theme>[] compatible with mui components sx prop.
+    Can you help me with that?
+*/
