487636ca71554b4b042c66bccd9d237955185a48
kit <kit.mavareik@gmail.com>
2024-09-19 15:32:14 +0300

    Simplify ThemeSwitch styling and add theme toggle functionality.
    Reduced redundant styles in ThemeSwitch.styles.ts to improve readability and maintainability. Integrated useThemeProvider hook in ThemeSwitch.tsx to enable theme mode toggle functionality.

--- src/design/organisms/ThemeSwitch/ThemeSwitch.tsx
+++ src/design/organisms/ThemeSwitch/ThemeSwitch.tsx
@@ -1,7 +1,10 @@
 import { Switch } from "@mui/material";
 
 import style from "./ThemeSwitch.styles";
+import { useThemeProvider } from "App/ThemeProvider/ThemeProvider.hooks.ts";
 
 export const ThemeSwitch = () => {
-  return <Switch sx={style.container} />;
+  const { toggleThemeMode } = useThemeProvider();
+
+  return <Switch sx={style.container} onChange={toggleThemeMode} />;
 };
