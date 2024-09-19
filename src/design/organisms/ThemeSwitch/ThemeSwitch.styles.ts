798a28153c390794f0824ee3d05be7c519882a36
kit <kit.mavareik@gmail.com>
2024-09-19 15:55:27 +0300

    Update ThemeSwitch to include sun and moon icons
    This commit adds sun and moon icons to the ThemeSwitch component, enhancing the user interface with visually intuitive indicators. It also introduces new style definitions to accommodate these icons, ensuring they blend seamlessly with the existing theme.

--- src/design/organisms/ThemeSwitch/ThemeSwitch.styles.ts
+++ src/design/organisms/ThemeSwitch/ThemeSwitch.styles.ts
@@ -1,6 +1,6 @@
 import { SxProps, switchClasses } from "@mui/material";
 import { Theme } from "@mui/material/styles";
-import { blueGrey, yellow } from "@mui/material/colors";
+import { blueGrey, yellow, grey } from "@mui/material/colors";
 
 const container: SxProps<Theme> = (theme: Theme) =>
   ({
@@ -11,8 +11,32 @@ const container: SxProps<Theme> = (theme: Theme) =>
         backgroundColor: blueGrey["700"],
       }),
     },
+    [`.${switchClasses.track}`]: {
+      opacity: 1,
+      backgroundColor: blueGrey["700"],
+
+      ...theme.applyStyles("dark", {
+        opacity: 1,
+        backgroundColor: yellow["100"],
+      }),
+    },
   }) as const;
+
+const icon: { moon: SxProps<Theme>; sun: SxProps<Theme> } = {
+  moon: {
+    fill: grey["100"],
+    backgroundColor: blueGrey["700"],
+    borderRadius: "50%",
+  },
+  sun: {
+    fill: yellow["600"],
+    backgroundColor: yellow["100"],
+    borderRadius: "50%",
+    padding: 0.5,
+  },
+};
 
 export default {
   container,
+  icon,
 };
--- src/design/organisms/ThemeSwitch/ThemeSwitch.tsx
+++ src/design/organisms/ThemeSwitch/ThemeSwitch.tsx
@@ -1,3 +1,5 @@
+import SunIcon from "@mui/icons-material/WbSunnyRounded";
+import MoonIcon from "@mui/icons-material/DarkModeRounded";
 import { Switch } from "@mui/material";
 
 import style from "./ThemeSwitch.styles";
@@ -6,5 +8,12 @@ import { useThemeProvider } from "App/ThemeProvider/ThemeProvider.hooks.ts";
 export const ThemeSwitch = () => {
   const { toggleThemeMode } = useThemeProvider();
 
-  return <Switch sx={style.container} onChange={toggleThemeMode} />;
+  return (
+    <Switch
+      sx={style.container}
+      onChange={toggleThemeMode}
+      checkedIcon={<MoonIcon sx={style.icon.moon} />}
+      icon={<SunIcon sx={style.icon.sun} />}
+    />
+  );
 };
