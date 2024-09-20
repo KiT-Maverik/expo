e7ec078a9319411e96c76ad116239f3cae8a2946
kit <kit.mavareik@gmail.com>
2024-09-20 13:54:08 +0300

    Add new routes and icons for additional components
    This commit introduces new routes and corresponding icons for the Copy to Clipboard, Countdown, Layout Detector, Login, Logout, and Viewport Detector components. The changes help expand navigational options in the application.

--- src/App/variables/routes.contants.ts
+++ src/App/variables/routes.contants.ts
@@ -8,7 +8,13 @@ export enum ROOT_ROUTES {
 }
 
 export enum HOOK_ROUTES {
+	COPY_TO_CLIPBOARD = 'copyToClipboard',
+	COUNTDOWN = 'countdown',
+	LAYOUT_DETECTOR = 'layoutDetector',
+	LOGIN = 'login',
+	LOGOUT = 'logout',
 	PASSWORD_VALIDATOR = 'passwordValidator',
+	VIEWPORT_DETECTOR = 'viewportDetector',
 }
 
 const root: { [key in ROOT_ROUTES]: Route } = {
@@ -16,7 +22,13 @@ const root: { [key in ROOT_ROUTES]: Route } = {
 }
 
 const hooks: { [key in HOOK_ROUTES]: Route } = {
+	[HOOK_ROUTES.COPY_TO_CLIPBOARD]: { href: '/copy-to-clipboard', title: 'Copy to Clipboard' },
+	[HOOK_ROUTES.COUNTDOWN]: { href: '/countdown', title: 'Countdown' },
+	[HOOK_ROUTES.LAYOUT_DETECTOR]: { href: '/layout-detector', title: 'Layout detector' },
+	[HOOK_ROUTES.LOGIN]: { href: '/login', title: 'Login' },
+	[HOOK_ROUTES.LOGOUT]: { href: '/logout', title: 'Logout' },
 	[HOOK_ROUTES.PASSWORD_VALIDATOR]: { href: '/password-validator', title: 'Password Validator' },
+	[HOOK_ROUTES.VIEWPORT_DETECTOR]: { href: '/viewport-detector', title: 'Viewport Detector' },
 }
 
 export const route = {
--- src/design/templates/layout/Drawer/items/Navigation.tsx
+++ src/design/templates/layout/Drawer/items/Navigation.tsx
@@ -1,13 +1,25 @@
+import CopyIcon from '@mui/icons-material/ContentCopyRounded'
 import HomeIcon from '@mui/icons-material/HomeRounded'
+import CountdownIcon from '@mui/icons-material/HourglassBottomRounded'
+import LoginIcon from '@mui/icons-material/LoginRounded'
+import LogoutIcon from '@mui/icons-material/LogoutRounded'
+import LayoutDetectorIcon from '@mui/icons-material/ViewCozyRounded'
 import PasswordValidatorIcon from '@mui/icons-material/PasswordRounded'
+import ViewportDetectorIcon from '@mui/icons-material/ScreenshotMonitorRounded'
 import { Divider, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
 import { ReactNode } from 'react'
 import { useNavigate } from 'react-router-dom'
 
-import { HOOK_ROUTES, route } from 'App'
+import { HOOK_ROUTES, route } from 'App/variables/routes.contants.ts'
 
 const icons: { [key in HOOK_ROUTES]: ReactNode } = {
-	passwordValidator: <PasswordValidatorIcon />,
+	[HOOK_ROUTES.COPY_TO_CLIPBOARD]: <CopyIcon />,
+	[HOOK_ROUTES.COUNTDOWN]: <CountdownIcon />,
+	[HOOK_ROUTES.LAYOUT_DETECTOR]: <LayoutDetectorIcon />,
+	[HOOK_ROUTES.LOGIN]: <LoginIcon />,
+	[HOOK_ROUTES.LOGOUT]: <LogoutIcon />,
+	[HOOK_ROUTES.PASSWORD_VALIDATOR]: <PasswordValidatorIcon />,
+	[HOOK_ROUTES.VIEWPORT_DETECTOR]: <ViewportDetectorIcon />,
 }
 
 export const Navigation = () => {
