a159a7dccc1d02d9fd8d385796b39267416d5c62
kit <kit.mavareik@gmail.com>
2024-09-20 12:08:56 +0300

    Remove Prettier and associated configurations
    This commit removes Prettier and its ESLint configurations from the project. Dependencies related to Prettier have been deleted, and corresponding configurations in `eslint.config.mjs` and `yarn.lock` have been removed for consistency.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,14 +2,12 @@ import globals from 'globals'
 import pluginJs from '@eslint/js'
 import tsEsLint from 'typescript-eslint'
 import pluginReact from 'eslint-plugin-react'
-// import pluginPrettier from 'eslint-plugin-prettier/recommended'
 
 export default [
 	{ files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
 	pluginJs.configs.recommended,
 	...tsEsLint.configs.recommended,
 	pluginReact.configs.flat.recommended,
-	// pluginPrettier,
 	{
 		languageOptions: { globals: globals.browser },
 		rules: {
